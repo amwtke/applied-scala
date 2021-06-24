3946419786989456eb779fce27e3191ddaf0df21
jin.xiao <jin.xiao@rea-group.com>
2021-06-24 15:47:30 +0800

    http4s

--- src/main/scala/com/reagroup/appliedscala/AppRoutes.scala
+++ src/main/scala/com/reagroup/appliedscala/AppRoutes.scala
@@ -26,7 +26,7 @@ class AppRoutes(fetchAllMoviesHandler: IO[Response[IO]],
 //    case GET -> Root / "movies" / LongVar(id) => fetchMovieHandler(id)
     case GET -> Root / "movies" / LongVar(id) :? OptionalEnrichedMatcher(optEnrich) =>
       if (optEnrich.contains(true)) fetchEnrichedMovieHandler(id) else fetchMovieHandler(id)
-    case req@POST -> Root / "movies" => saveMovieHandler(req)
-    case req@POST -> Root / "movies" / LongVar(id) / "reviews" => saveReviewHandler(id, req)
+    case req@ POST -> Root / "movies" => saveMovieHandler(req)
+    case req@ POST -> Root / "movies" / LongVar(id) / "reviews" => saveReviewHandler(id, req)
   }
 }
