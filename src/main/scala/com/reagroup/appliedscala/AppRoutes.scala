3682fb5c1bc6db2489692bcafa68a0dc1b5bf5c2
jin.xiao <jin.xiao@rea-group.com>
2021-06-24 15:51:19 +0800

    http4s

--- src/main/scala/com/reagroup/appliedscala/AppRoutes.scala
+++ src/main/scala/com/reagroup/appliedscala/AppRoutes.scala
@@ -23,10 +23,10 @@ class AppRoutes(fetchAllMoviesHandler: IO[Response[IO]],
   val openRoutes: HttpRoutes[IO] = HttpRoutes.of {
     case ->(GET, /(Root, "movies")) => fetchAllMoviesHandler
     case PUT -> Root / "movies" => fetchAllMoviesHandler
-//    case GET -> Root / "movies" / LongVar(id) => fetchMovieHandler(id)
+    //    case GET -> Root / "movies" / LongVar(id) => fetchMovieHandler(id)
     case GET -> Root / "movies" / LongVar(id) :? OptionalEnrichedMatcher(optEnrich) =>
       if (optEnrich.contains(true)) fetchEnrichedMovieHandler(id) else fetchMovieHandler(id)
-    case req@ POST -> Root / "movies" => saveMovieHandler(req)
-    case req@ POST -> Root / "movies" / LongVar(id) / "reviews" => saveReviewHandler(id, req)
+    case req@POST -> Root / "movies" => saveMovieHandler(req)
+    case req@POST -> Root / "movies" / LongVar(id) / "reviews" => saveReviewHandler(id, req)
   }
 }
